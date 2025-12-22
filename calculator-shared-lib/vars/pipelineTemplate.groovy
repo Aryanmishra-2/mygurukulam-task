d70e25774c41d5f4c6ada8f2e181b9fe061d06b1
Aryanmishra-2 <aryan.mishra@mygurukulam.co>
2025-12-22 22:06:45 +0530

    Update and rename calculator.groovy to pipelineTemplate.groovy

---
+++ calculator-shared-lib/vars/pipelineTemplate.groovy
@@ -0,0 +1,32 @@
+import com.company.utils.*
+
+def call(Map config) {
+    try {
+
+        stage('Clean Workspace') {
+            new CleanWorkspace(this).run()
+        }
+
+        stage('Publish Success Report') {
+            new PublishReport(this).send(
+                'SUCCESS',
+                config.email
+            )
+        }
+
+    } catch (err) {
+
+        stage('Publish Failure Report') {
+            new PublishReport(this).send(
+                'FAILURE',
+                config.email
+            )
+        }
+
+        throw err
+
+    } finally {
+        echo "CD Pipeline execution completed."
+    }
+}
+
