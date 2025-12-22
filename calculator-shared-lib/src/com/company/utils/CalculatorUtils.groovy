78a5ba09bc51e3f1a0e9006e3e6d1c7d5622abd2
Aryanmishra-2 <aryan.mishra@mygurukulam.co>
2025-12-22 14:50:41 +0530

    Update CalculatorUtils.groovy

--- calculator-shared-lib/src/com/company/utils/CalculatorUtils.groovy
+++ calculator-shared-lib/src/com/company/utils/CalculatorUtils.groovy
@@ -1,1 +1,24 @@
+package com.company.utils
+
+class CalculatorUtils implements Serializable {
+
+    static double add(double a, double b) {
+        return a + b
+    }
+
+    static double subtract(double a, double b) {
+        return a - b
+    }
+
+    static double multiply(double a, double b) {
+        return a * b
+    }
+
+    static double divide(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed")
+        }
+        return a / b
+    }
+}
 
